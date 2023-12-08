e6eb136db3862158c16f8b8cf74f932078fcf98a
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-08 14:21:24 +0700

    feat(exercise): add completed exercise

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,11 @@
 //Exercise 1: Add Strings
 
 const addStrings = function (num1, num2) {
-  //Start coding here
+  let number1 = Number(num1);
+  let number2 = Number(num2);
+  let sum = number1 + number2;
+
+  return String(sum);
 };
 
 const result1 = addStrings("11", "123");
@@ -11,3 +15,19 @@ const result3 = addStrings("0", "0");
 console.log(result1); // "134"
 console.log(result2); // "533"
 console.log(result3); // "0"
+
+/* ปัญหา
+   - ให้เอา Value Type ที่เป็น String มาบวกกัน โดยการใช้ฟังก์ชัน และให้ฟังก์ชัน Return ผลบวกที่เป็น Type String */
+
+/* วิธีแก้ปัญหา
+   - เปลี่ยนจาก String เป็น Number และสร้างตัวแปรมาเก็บค่า
+   - เอาตัวแปรทั้งสองที่แปลงเป็น number แล้วมาบวกกัน และสร้างตัวแปรมาเก็บค่า
+   - return ผลลัพธ์พร้อมกับแปลงจาก String เป็น Number                                           */
+
+// สามารถปรับปรุงให้โค้ดสั้นลงได้แบบนี้
+
+/* const addStrings = function (num1, num2) {
+  let sum = Number(num1) + Number(num2);
+
+  return String(sum);
+}; */
--- ex-2.js
+++ ex-2.js
@@ -1,13 +1,27 @@
 //Exercise 2: Valid Palindrome
 
 const isPalindrome = function (s) {
-  //Start Coding here
+  let cleanedStr = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let reversedStr = cleanedStr.split("").reverse().join("");
+  return cleanedStr === reversedStr;
 };
 
-const result1 = isPalindrome("A man, a plan, a canal: Panama");
-const result2 = isPalindrome("race a car");
-const result3 = isPalindrome(" ");
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(isPalindrome("race a car")); // false
+console.log(isPalindrome(" ")); // true
 
-console.log(result1); // true
-console.log(result2); // false
-console.log(result3); // true
+/* ปัญหา
+   - ให้ตรวจสอบว่า String ที่กำหนดเป็น Palindrome หรือไม่ ถ้าใช่ให้แสดงผลลัพธ์เป็น true ถ้าไม่ใช่ให้เป็น false          */
+
+/* วิธีแก้ปัญหา
+   - 1) สร้างฟังก์ชัน เพื่อเอามารับค่า String แล้วเอาไปตรวจสอบว่าเป็น Palindrome หรือไม่
+     - จะเช็คว่าเป็น Palindrome หรือไม่ จะต้องเปลี่ยนทุกตัวเป็นตัวเล็ก และลบสัญลักษณ์ที่ไม่ใช่ตัวอักษรออกทั้งหมด และเช็คว่ากลับด้านแล้วจะอ่านได้เหมือนเดิมมั้ย
+   - 2) เปลี่ยนทุกตัวเป็นตัวเล็ก และลบสัญลักษณ์ที่ไม่ใช่ตัวอักษรออกทั้งหมด
+     - ใช้ฟังก์ชัน .replace(/[^a-zA-Z0-9]/g, "") เพื่อลบทุกตัวที่ไม่ใช่ a-z, A-Z, 0-9
+     - ใช้ฟังก์ชัน .toLowerCase() เพื่อทำให้ทุกตัวเป็นตัวเล็ก
+     - สร้างตัวแปรมารับค่า          
+   - 3) กลับด้านข้อมูล
+    - ใช้ .split("") เพื่อทำให้เป็น Array แล้วใช้ .reverse () เพื่อกลับด้านข้อมูลใน Array จากนั้นใช้ .join("") เพื่อเอาข้อมูลใน Array มารวมกันเป็น String
+    - สร้างตัวแปรมารับค่า
+   - 4) เปรียบเทียบตัวแปรทั้งสองว่าเท่ากันหรือไม่ โดยการใช้ Comparison Opertor === เพื่อจะเอาคำตอบเป็น Boolean จากนั้น return ผลลัพธ์
+*/
